ec0338814867cd6281e77ac9b699899002b24eac
unknown <louiecervantes@wvsu.edu.ph>
2023-04-10 18:52:08 +0800

    save changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ def app():
         if st.button('Submit'):    
                    
             # Define the number of words in each chunk
-            chunk_size = 120
+            chunk_size = 80
             
             text_chunks = chunker(user_input, chunk_size)
 
